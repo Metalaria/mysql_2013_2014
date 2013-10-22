0d199c00639c57ba7fb78311d1af3a3c7deeafda
Metalaria <gonzalo.mejias.moreno@gmail.com>
2013-10-22 10:20:20 +0200

    Update 22oct.sql

--- 22oct.sql
+++ 22oct.sql
@@ -1,3 +1,4 @@
+G
 
 GRANT 
 SELECT,INSERT,UPDATE,DELETE,INDEX,ALTER,CREATE,DROP,RELOAD,SHUTDOWN,PROCESS,FILE
@@ -14,6 +15,14 @@ SELECT
 WITH GRANT OPTION;
 
 --------------------------------------------------------------------------------------
+
+/************************************************************************************************
+*												*
+* Permita crear un usuario llamado 'invitado' que pueda realizar operaciones de selección,	*
+* inserción, actualización y borrado sobre cualquier tabla de la base de datos . 		*
+* Limita sus conexiones por hora a 5. Utiliza la instrucción GRANT				*
+*												*
+*************************************************************************************************/
 
 GRANT 
 SELECT,INSERT,UPDATE,DELETE,INDEX,ALTER,CREATE
@@ -59,7 +68,12 @@ end
 delmiter;
 
 -------------------------------------------------------------------------
-
+/*******************************************************************************************
+*											   *
+* Crea una función denominada 'esimpar' que a partir de un número entero devuelva 1    	   *
+* si el número introducido es impar y 0 si no lo es.					   *
+*											   *
+********************************************************************************************/
 create function esimpar (in numero int)
 	returns int
 	deterministic
@@ -73,10 +87,22 @@ end;
 
 --------------------------------------------------------------------
 
+/****************************************************************************************
+* Prueba la función anterior desde la consola por ejemplo con SET @x=esimpar(50); 	*
+* Después comprueba el valor de @x con SELECT @x; 					*
+* Observa que no se ha utilizado CALL para ejecutar la función.				*
+*****************************************************************************************/
+
 set @n=esimpar(7)
 
 -------------------------------------------------------------
 
+/**************************************************************************
+Crea un procedimiento almacenado denominado muestra_estado que a partir   *
+de un número entero devuelva la expresión sobre si es impar o par, 	  *
+por ejemplo para 7 y 8 "7 es impar" y "8 es par" respectivamente. 	  *
+Utiliza dentro del procedimiento la función 'esimpar'			  *
+***************************************************************************/
 create procedure muestra_estado (in numero int)
 begin
 if (esimpar(numero))
@@ -88,13 +114,65 @@ end if;
 end;
 
 -------------------------------------------------------------------
+/********************************************************
+*							*
+* Prueba el procedimiento anterior utilizando CALL	*
+*							*
+*********************************************************/
 
 call muestra_estado(@x)
 call muestra_estado(75)
 
 ---------------------------------------------------------
 
+/****************************************************************************************
+*											*
+* Crea una función denominada 'f_comprueba' que a partir de un				*
+* número entero devuelva 1 si es múltiplo de 2, 2 si es múltiplo de 3, 			*
+* 3 si es múltiplo de 4 y 0 si nada de lo anterior. Se recomienda usar un CASE.		*
+*											*
+*****************************************************************************************/
+
+
 create function f_comprueba (numero int) returns int
 deterministic
 begin
 case
+when (numero%2=0) then returns 1;
+when (numero%3=0) then returns 2;
+when (numero%4=0) then returns 3;
+else return 0;
+end case;
+end;
+
+-------------------------------------------------------------------
+
+
+handler;
+create procedure insertar_ciclista (in ..... , out mensaje varchar(45))
+modifies sql data
+begin
+	declare continue handler for 1062 set mensaje = 'DNI de ciclista ya insertado';
+	set mensaje= 'ciclista insertado con exito';
+	insert into ciclistas (....) values (....);
+end;
+
+-------------------------------------------------------------------------------
+
+create procedure insertar_ciclista (in .....)
+modifies sql data
+begin
+	begin
+		declare clave_duplicada int default 0;
+		declare exit handler for 1062 set clave_duplicada = 1;
+		insert into ciclistas (....) values ;
+	end;
+if clave_duplicada = 1 then
+	select concat ....
+else
+	select concat ....
+end if;
+end;
+
+
+
